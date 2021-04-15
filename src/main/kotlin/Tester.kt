07d127a244ace196a451fbc03698592ae8918d0a
James <goude47@gmail.com>
2021-04-15 20:09:40 +0100

    Save me BM

--- src/main/kotlin/Tester.kt
+++ src/main/kotlin/Tester.kt
@@ -1,26 +1,29 @@
-class Tester(private val objectt: Any) {
+class TestBuilder {
+
+    private val testData = mutableListOf<Any>()
+    private var objectt: Any = 123
+
+    fun testData(vararg data: Any) = testData.addAll(data)
+
+    fun toTest(clazz: Any) { objectt = clazz }
 
     @OptIn(ExperimentalStdlibApi::class)
-    private val functionName = objectt.javaClass.simpleName.split("Solution")[0].replaceFirstChar { it.toLowerCase() }
+    fun run(): Any {
 
-    fun runTest(vararg testData: Any) {
+        println(objectt)
 
+        val functionName = objectt.javaClass.simpleName.split("Solution")[0].replaceFirstChar { it.toLowerCase() }
+        println(functionName)
         val method = objectt.javaClass.declaredMethods.first { it.name == functionName }
-        val paramTypes = method.parameterTypes
 
-        val newData = mutableListOf<Any>(testData.indices.forEach { paramTypes[it].cast(testData[it]) })
-
-        val result = method.invoke(objectt.javaClass.constructors[0].newInstance(), newData)
-        println(result)
+        return method.invoke(objectt, *testData.toTypedArray())
 
     }
 
 }
 
-fun test(toTest: Any): Boolean {
+fun test(initializer: TestBuilder.() -> Unit): TestBuilder {
 
-    if (toTest !is Solution) return false
-
-    return true
+    return TestBuilder().apply { initializer }
 
 }
--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,6 +1,15 @@
 fun main(args: Array<String>) {
     println("Hello World!")
-    Tester(TwoSumSolution()).runTest(intArrayOf(2, 7, 11, 15), 9)
+
+    val passing = test {
+        toTest(TwoSumSolution())
+        println("Object loaded")
+        testData(intArrayOf(2, 7, 11, 15))
+        testData(9)
+    }.run()
+
+    println("Passing? $passing")
+
     //TwoSumSolution().twoSum(intArrayOf(2, 7, 11, 15), 9)
 }
 
